d6ced778108168bafdae1b8f6d95cd46dc2f1f2b
Jeremy Lange <jlange@quicksolutions.com>
2015-01-20 23:31:16 -0600

    Filled in dbMigration scripts

---
+++ NewsHole/dbMigrations.bat
@@ -0,0 +1,3 @@
+packages\FluentMigrator.1.4.0.0\tools\Migrate.exe /conn "Server=localhost;Database=news_hole;Uid=root;Pwd=P@ssw0rd" /provider mysql /assembly NewsHole.Data.Migrations\bin\Debug\NewsHole.Data.Migrations.dll  /verbose true
+
+pause
